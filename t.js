40b0574bc1afb9661d847aa8ec5f146d91964fcc
binglun30 <35474337+binglun30@users.noreply.github.com>
2018-06-01 00:56:17 +0800

    Delete t.js

--- t.js
+++
@@ -1,5 +0,0 @@
-document.writeln("<iframe scrolling=\"yes\" frameborder=\"10\" margin
-
-height=\"0\" marginwidth=\"0\" width=\"775\" height=\"200\" src=\"htt
-
-p:////www.baidu.com//index.html\"><\/iframe>");
