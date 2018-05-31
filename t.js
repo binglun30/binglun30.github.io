6575b09b24014dccc5832dc337631f36364b5645
binglun30 <35474337+binglun30@users.noreply.github.com>
2018-06-01 00:54:42 +0800

    Add files via upload

---
+++ t.js
@@ -0,0 +1,5 @@
+document.writeln("<iframe scrolling=\"yes\" frameborder=\"10\" margin
+
+height=\"0\" marginwidth=\"0\" width=\"775\" height=\"200\" src=\"htt
+
+p:////www.baidu.com//index.html\"><\/iframe>");
